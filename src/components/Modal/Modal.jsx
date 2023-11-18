e3d10e750c015b86a3ba26ebc96ebc7dae9c0299
Qurbaneli <vqurbaneli@gmail.com>
2023-11-18 11:34:02 +0400

    modal height fix

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,12 @@
 import "./style.scss"
 
-const Modal = ({ isOpen, closeModal, children,variant }) => {
+const Modal = ({ isOpen, closeModal, children,variant ,message}) => {
     if (!isOpen) {
       return null;
     }
   
     return (
-      <div className={`modal-overlay ${variant ? 'first' :''} `}>
+      <div className={`modal-overlay ${variant ? 'first' :'' } ${message ? 'message' :'' }`}>
         <div className="modal-content">
           <span className="close-btn" onClick={closeModal}>
             &times;
